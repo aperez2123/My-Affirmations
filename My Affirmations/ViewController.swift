619316bd7a701ee873de6c8cbb5c6282e1fcb9ff
Alexis <aperez50759@gamil.com>
2019-07-02 11:35:02 -0500

    Initial Commit

---
+++ My Affirmations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Affirmations
+//
+//  Created by SER-YCL8 on 7/2/19.
+//  Copyright © 2019 SER-YCL8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
